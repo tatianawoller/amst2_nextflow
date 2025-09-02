5143a380a95c959ad41f21e056ef6d4e79264760
Tatiana Woller <103046100+tatianawoller@users.noreply.github.com>
2025-09-02 13:01:37 +0200

    Update amst.py

--- python_scripts_poc/amst.py
+++ python_scripts_poc/amst.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 
 """
 Proof of concept to run prealign like in AMST2
@@ -9,7 +8,6 @@ from squirrel.workflows.amst import amst_workflow
 import os
 from squirrel.workflows.elastix import apply_multi_step_stack_alignment_workflow
 import yaml
-#import glob
 
 def load_parameter_yaml(parameter_yaml):
 
@@ -20,29 +18,17 @@ def load_parameter_yaml(parameter_yaml):
 
 def run_amst():
     
-    parameter_dict = load_parameter_yaml("/home/twoller/AMST2/squirrel/params-amst.yaml")
+    parameter_dict = load_parameter_yaml("/home/tatiana/squirrel/params-amst.yaml")
     output_dirpath = parameter_dict['general']['output_dirpath']
     input_dirpath = parameter_dict['general']['input_dirpath']
     pre_align_dirpath = parameter_dict['general']['pre_align_dirpath']
-
-
-    # FinalGridSpacingInPhysicalUnits:256 GridSpacingSchedule:4.0,3.0,2.0,1.0 MaximumStepLength:0.5 MaximumNumberOfIterations:1024
-    # if  not parameter_dict['general']['pre_align_transforms'] and parameter_dict['amst']['elastix_parameter_file'] != 'auto':
-    make_elastix_default_parameter_file_workflow(
-             out_filepath=os.path.join(output_dirpath, "elastix-params-amst-gs256.txt"),
-             transform='bspline')  # or 'rigid', 'affine', 'translation'
-    #        # final_grid_spacing_in_physical_units=256,
-    #     #    grid_spacing_schedule=[4.0,3.0,2.0,1.0],
-    #        # maximum_number_of_iterations=1024,
-    #       #  maximum_step_length=0.5
-    # )
-    #     elastix_parameters=os.path.join(output_dirpath, "elastix-params-amst-gs256.txt")
-    #     parameter_dict['amst']['elastix_parameters'] = elastix_parameters
+    pre_align_transforms = parameter_dict['general']['pre_align_transforms']
     
 
-    # just for testing puporse
+    make_elastix_default_parameter_file_workflow(
+             out_filepath=os.path.join(output_dirpath, "elastix-params-amst-gs256.txt"),
+             transform='bspline')
 
-   # elastix_param_files = glob.glob("elastix_params_amst_dummy.txt")
     amst_workflow(
         pre_align_dirpath,
         os.path.join(output_dirpath,'amst-transforms'),
@@ -72,9 +58,8 @@ def run_amst():
             target_image_shape=None, 
             z_range=None,       # Use all slices
             n_workers=1,        
-            verbose=False        # Was set to True in the original code
+            write_result=True
         )
 
 if __name__ == "__main__":
     run_amst()
-    
